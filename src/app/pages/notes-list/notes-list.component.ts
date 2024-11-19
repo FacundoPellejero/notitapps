d131dcb6058e0bb8dc10b0739a11f43dec59f1a5
Pelle <facundopellejero.nt@gmail.com>
2024-11-18 22:07:39 -0300

    ui notes list

---
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NoteCardComponent } from "../../note-card/note-card.component";
+
+@Component({
+  selector: 'app-notes-list',
+  standalone: true,
+  imports: [NoteCardComponent],
+  templateUrl: './notes-list.component.html',
+  styleUrl: './notes-list.component.scss'
+})
+export class NotesListComponent {
+  
+  
+  
+  constructor() {
+    
+  }
+  ngOnInit(){
+
+  }
+
+}
