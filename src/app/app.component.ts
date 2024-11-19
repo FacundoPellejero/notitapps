2a0db25159014571e332025ffb31da011c9c7760
Pelle <facundopellejero.nt@gmail.com>
2024-11-18 22:05:27 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'notes-front-without-ssr';
+  title = 'notes-front';
 }
