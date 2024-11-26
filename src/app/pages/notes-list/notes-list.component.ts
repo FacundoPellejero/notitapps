cd0330543cf45a38dca0bf01a8b1415cd45a8faa
Pelle <facundopellejero.nt@gmail.com>
2024-11-25 22:09:53 -0300

    notes-list crud control

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NoteCardComponent } from "../../note-card/note-card.component";
+import { Note } from '../../shared/note.model';
+import { NotesService } from '../../shared/notes.service';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-notes-list',
   standalone: true,
-  imports: [NoteCardComponent],
+  imports: [NoteCardComponent, NgFor, RouterLink],
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.scss'
 })
-export class NotesListComponent {
+export class NotesListComponent implements OnInit {
   
+  notes: Note[] = new Array<Note>();
   
-  
-  constructor() {
+  constructor(private notesService: NotesService) {
     
   }
   ngOnInit(){
-
+    //Traemos todas las notas desde el servicio
+    this.notes = this.notesService.getAll();
   }
 
+  deleteNote(id: number){
+    this.notesService.delete(id);
+  }
 }
